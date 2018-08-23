147bacd069f6af7934679ebe49f28cbf4ec66019
gauravkad8 <kad.gaurav.gaurav447@gmail.com>
2018-08-23 19:54:59 +0530

    history Page

--- Aboutus/history.php
+++ Aboutus/history.php
@@ -17,96 +17,50 @@
 <link href="whatsApp.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
-<style>
-    .w3-sidebar a {font-family: "Roboto", sans-serif}
-    body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
-</style>
-<body class="w3-content" style="max-width:1200px">
+<body>
 <?php include '../header.php';?>
-<!-- Sidebar/menu -->
-<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px;height:450px;" id="mySidebar">
-    <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold;margin-top:60px">
-       <?php include 'aboutus_link.php';?>
-    </div>
-</nav>
+<br><br><br>
 
-<!-- Top menu on small screens -->
-<header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
-    <div class="w3-bar-item w3-padding-24 w3-wide"><img src="img/logo.png"></div>
-    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
-</header>
-
-<!-- Overlay effect when opening sidebar on small screens -->
-<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-
-<!-- !PAGE CONTENT! -->
-<div class="w3-main" style="margin-left:250px">
-
-    <!-- Push down content on small screens -->
-    <div class="w3-hide-large" style="margin-top:83px"></div>
-
-    <!-- Top header -->
-      <div class="container">
-           <div class="row">
-		   
-	<p><img src="img/history/1.png" align="right" Style="margin-left:120px 0px"><p>
-<br><br><br>	
-<h1>COMPANY HISTORY AND TIMELINE</h1>
-<p>1)Idea (June 2016) – Eureka, Idea of 3D printing business.<br>
-2)Market survey (August 2016) <br>
-3)Training at MCED (October 2016)<br>
-4)Planning and development (December 2016)<br>
-5)Company Incorporation as IPRO3D TECHNOLOGIES LLP (October 2017)<br>
-6)Plant set up in Aurangabad (October 2017)<br>
-7)Website launch and India 1st Additive Manufacturing 3D Printing Online Marketplace (March 2018)<br>
-8)Incubated at MAGIC (Marathwada Accelerator for Growth and Incubation Council) Incubation Center by CMIA (Chamber of Marathwada Industries and Agriculture) (May 18)<br>
-9)Recognized as a Startup by DIPP (Department of Industrial Policy and Promotion) under Ministry of Commerce and Industries, Government of India. (May 18)<br>
-10)Solidworks partners as CAD partner with IPRO3D TECHNOLOGIES LLP (July 18)<br>
-
-
-             </div>
-        </div>
-	
-
-    
-
-
-    <!-- End page content -->
+<div id="menu_flip" >
+    <button class="btn btn-primary">More Menu</button>
+</div>
+<div id="menu_panel">
+    <?php include 'aboutus_link.php';?>
 </div>
 
-<!-- Newsletter Modal -->
+<div class="container">
+    <div class="row" style="color: darkslategrey;background-color: whitesmoke;padding: 20px;">
+        <h1 style="text-align: center;font-weight: bolder;"><i>COMPANY HISTORY AND TIMELINE</i></h1>
+<ol style="font-weight: bold;margin-top: 5px;">
+ <li>Idea (June 2016) – Eureka, Idea of 3D printing business.</li>
+<li>Market survey (August 2016)</li>
+<li>Training at MCED (October 2016)</li>
+<li>Planning and development (December 2016)</li>
+<li>Company Incorporation as IPRO3D TECHNOLOGIES LLP (October 2017)</li>
+<li>Plant set up in Aurangabad (October 2017)</li>
+<li>Website launch and India 1st Additive Manufacturing 3D Printing Online Marketplace (March 2018)</li>
+<li>Incubated at MAGIC (Marathwada Accelerator for Growth and Incubation Council) Incubation Center by CMIA (Chamber of Marathwada Industries and Agriculture) (May 18)</li>
+<li>Recognized as a Startup by DIPP (Department of Industrial Policy and Promotion) under Ministry of Commerce and Industries, Government of India. (May 18)</li>
+<li>Solidworks partners as CAD partner with IPRO3D TECHNOLOGIES LLP (July 18)</li>
+</ol>
+        <br><br>
+    </div>
+
+</div>
+<br>
+<div class="container-fluid">
+    <div class="row">
+    <?php include'../footer.php';?>
+    </div>
+</div>
 
 <script>
-    // Accordion
-    function myAccFunc() {
-        var x = document.getElementById("demoAcc");
-        if (x.className.indexOf("w3-show") == -1) {
-            x.className += " w3-show";
-        } else {
-            x.className = x.className.replace(" w3-show", "");
-        }
-    }
+    $(document).ready(function(){
+        $("#menu_flip").click(function(){
+            $("#menu_panel").slideToggle("slow");
+        });
 
-    // Click on the "Jeans" link on page load to open the accordion for demo purposes
-    document.getElementById("myBtn").click();
-
-
-    // Script to open and close sidebar
-    function w3_open() {
-        document.getElementById("mySidebar").style.display = "block";
-        document.getElementById("myOverlay").style.display = "block";
-    }
-
-    function w3_close() {
-        document.getElementById("mySidebar").style.display = "none";
-        document.getElementById("myOverlay").style.display = "none";
-    }
+    });
 </script>
-<footer id="footer">
-<div class="container">
-    <?php include'../footer.php';?>
-</div>
-    </footer>
-
 </body>
 </html>
