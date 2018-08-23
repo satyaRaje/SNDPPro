527401ddd63b86b2c5292af926743b33889cbf0c
Nikita <nikitatk15@gmail.com>
2018-08-24 00:09:45 +0530

    reverse done

--- Services/reverse.php
+++ Services/reverse.php
@@ -19,143 +19,73 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 
-<style>
-    .w3-sidebar a {font-family: "Roboto", sans-serif}
-    body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
-</style>
 <body class="w3-content" style="max-width:1200px">
 
 <!-- Sidebar/menu -->
-<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
-    <div class="w3-container w3-display-container w3-padding-16">
-        <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
-        <h3 class="w3-wide"><b><img src="img/logo.png"></b></h3>
+<?php include '../header.php';?>
+<br><br><br>
+<div class="container">
+    <div class="row">
+        <h2 style="text-align: center;"><b>Reverse Engineering</b></h2>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+
+                <div class="item active">
+                    <img src="img/banner1.jpg" alt="Image">
+                    <div class="carousel-caption">
+                        <!-- <h1>MAHARASHTRA VOCATIONAL TRAINING CENTER</h1>
+                         <p> Affiliated to Maharashtra State Board of Vocational Education Examination, Mumbai </p>
+                         <h3>Institute Code: 170523</h3> -->
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="img/banner2.jpg" alt="Image">
+                    <div class="carousel-caption">
+                        <!--<h1>MAHARASHTRA VOCATIONAL TRAINING CENTER</h1>
+                        <p> Affiliated to Maharashtra State Board of Vocational Education Examination, Mumbai </p>
+                        <h3>Institute Code: 170523</h3> -->
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
     </div>
-    <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
-       <?php include 'template_link.php';?>
-     </div>
-
-</nav>
-
-<!-- Top menu on small screens -->
-<header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
-    <div class="w3-bar-item w3-padding-24 w3-wide"><img src="img/logo.png"></div>
-    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
-</header>
-
-<!-- Overlay effect when opening sidebar on small screens -->
-<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-
-<!-- !PAGE CONTENT! -->
-<div class="w3-main" style="margin-left:250px">
-
-    <!-- Push down content on small screens -->
-    <div class="w3-hide-large" style="margin-top:83px"></div>
-
-    <!-- Top header -->
-      <div class="container" align="left" style="margin:0px 300px">
-           <div class="row">
-           </br>
-		   </br>
-           <h1><b></b></h1>
-           </div>
-      </div>
-      <div class="container" align="left" style="margin:20px 50px">
-           <div class="row">
-           </br>
-		   </br>
-           <h4><b>Reverse Engineering:</b></h4>
-           </div>
-      </div>
-	
-<div id="myCarousel" class="carousel slide" data-ride="carousel" align="left" style="margin:50px">
-
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-    </ol>	
-	   <div class="carousel-inner" role="listbox">
-
-        <div class="item active">
-            <img src="img/banner1.jpg" alt="Image">
-            <div class="carousel-caption">
-               <!-- <h1>MAHARASHTRA VOCATIONAL TRAINING CENTER</h1>
-                <p> Affiliated to Maharashtra State Board of Vocational Education Examination, Mumbai </p>
-                <h3>Institute Code: 170523</h3> -->
-            </div>
-        </div>
-
-        <div class="item">
-            <img src="img/banner2.jpg" alt="Image">
-            <div class="carousel-caption">
-                <!--<h1>MAHARASHTRA VOCATIONAL TRAINING CENTER</h1>
-                <p> Affiliated to Maharashtra State Board of Vocational Education Examination, Mumbai </p>
-                <h3>Institute Code: 170523</h3> -->
-            </div>
-        </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
-   </div>
-	  
-	  
-	  
-      <div class="container" align="left" style="margin:0px 50px">
-           <div class="row">
-           <p>Reverse engineering is a method used to understand an item and its interaction with various components. It is helpful when you want to create an exact copy or customize according to your needs of a common product with a 3D printer.</br></br>
-
-
-           </div>
-      </div>
-	  <!-- Footer -->
-    <footer class="w3-padding-64 w3-light-grey w3-small w3-center" id="footer">
-
-
-
-    </footer>
-
-
-
-    <!-- End page content -->
 </div>
 
-<!-- Newsletter Modal -->
-
-<script>
-    // Accordion
-    function myAccFunc() {
-        var x = document.getElementById("demoAcc");
-        if (x.className.indexOf("w3-show") == -1) {
-            x.className += " w3-show";
-        } else {
-            x.className = x.className.replace(" w3-show", "");
-        }
-    }
-
-    // Click on the "Jeans" link on page load to open the accordion for demo purposes
-    document.getElementById("myBtn").click();
 
 
-    // Script to open and close sidebar
-    function w3_open() {
-        document.getElementById("mySidebar").style.display = "block";
-        document.getElementById("myOverlay").style.display = "block";
-    }
+<div class="container" style="padding: 20px;">
+    <div class="row" style="font-weight: bolder;">
+        Reverse engineering is a method used to understand an item and its interaction with various components. It is helpful when you want to create an exact copy or customize according to your needs of a common product with a 3D printer.</br></br>
+    </div>
+</div>
+<!-- Footer -->
+<div class="container-fluid">
+    <div class="row">
+        <?php include '../footer.php';?>
+    </div>
 
-    function w3_close() {
-        document.getElementById("mySidebar").style.display = "none";
-        document.getElementById("myOverlay").style.display = "none";
-    }
-</script>
+</div>
+
+
+
+<!-- End page content -->
 
 </body>
 </html>
