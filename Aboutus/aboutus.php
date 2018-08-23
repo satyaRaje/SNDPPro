165c0ecdccfed60352668927a14be79cd9fe7ef0
gauravkad8 <kad.gaurav.gaurav447@gmail.com>
2018-08-23 19:11:06 +0530

    About us Page

--- Aboutus/aboutus.php
+++ Aboutus/aboutus.php
@@ -18,7 +18,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 
-<body class="w3-content" style="max-width:1500px">
+<body>
 <?php include '../header.php';?>
 <!-- Sid-ebar/menu -->
 <br><br><br>
@@ -29,46 +29,44 @@
 <div id="menu_panel">
     <?php include 'aboutus_link.php';?>
 </div>
-<!-- Top menu on small screens -->
-
-<!-- Overlay effect when opening sidebar on small screens -->
-<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-
-<!-- !PAGE CONTENT! -->
-
-    <!-- Push down content on small screens -->
 
     <!-- Top header -->
 	<div class="container">
-           <div class="row">
+           <div class="row" style="color: darkslategrey;background-color:whitesmoke;">
 
                <h1 style="text-align: center;">About Us</h1>
 
-               <p>
+               <p style="text-align:justify;padding: 20px;">
                    &nbsp;&nbsp;We IPRO3D Technologies LLP is a 3D printing start-up located at Aurangabad, Maharashtra, India which is almost in the center of India geographically.We have a very good and passionate team of Engineers working towards the same goal.We provide professional Services online
-                   <br> and offline for on demand production of prototypes,end use products,as well as batch production for products, etc using additive manufacturing technology also popularly known as 3D printing.<br>
+                    and offline for on demand production of prototypes,end use products,as well as batch production for products, etc using additive manufacturing technology also popularly known as 3D printing.<br>
 
                    &nbsp;&nbsp;&nbsp;IPRO3D provides access to wide range of materials, finishes, techniques and manufacturing processes as well as superior optimization tools for file repair and analysis. We provide delivery of our 3d printed and traditionally manufactured products PAN India. We <br>
 
-                   also process worldwide delivery according to the order quantity.<br>
-                   <li>IPRO3D a socially responsible business:<br>
-                       1)The company runs with a strong focus on Corporate Social Responsibility.<br>
-                       2)The founders have entrenched their commitment to social issues in the company’s character. <br>
-                       The style of management strives to achieve success on a daily basis, but also in long term.<br>
-                       <br>
+                   also process worldwide delivery according to the order quantity.
 
+               <h3 style="font-weight: bold;">IPRO3D a socially responsible business:</h3>
+               <ol>
+                   <li>The company runs with a strong focus on Corporate Social Responsibility.</li>
+                   <li>The founders have entrenched their commitment to social issues in the company’s character.</li>
+                   <li>The style of management strives to achieve success on a daily basis, but also in long term.</li>
 
-               <li>Report abusive content:<br>
+               </ol>
+<br>
+              <p style="color: lightslategray;font-weight: bold; "><i> Report abusive content:<br>
                    If you think that this website presents abusive content, you may inform us at report@ipro3d.io
-
+                  </i>
                </p>
            </div>
 	</div>
 	<!-- Footer -->
     
-
+<br><br>
 <!-- Newsletter Modal -->
+<div class="container-fluid">
+<div class="row">
 <?php include '../footer.php';?>
+</div>
+</div>
 <script>
     // Accordion
     function myAccFunc() {
