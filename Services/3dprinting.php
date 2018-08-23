189334d7366acb28f6bd8f4a14c1e5c8d6dcd307
data <joshipoonam207@gmail.com>
2018-08-23 15:18:17 -0400

    p

--- Services/3dprinting.php
+++ Services/3dprinting.php
@@ -1,14 +1,9 @@
 <!DOCTYPE html>
 <html>
-<title>IPRO3D-ABOUT_US-Authentic</title>
+<title>W3.CSS Template</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -18,147 +13,281 @@
 <link href="whatsApp.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
-
 <style>
-    .w3-sidebar a {font-family: "Roboto", sans-serif}
-    body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
+    /* Full height image header */
+    .bgimg-1 {
+        background-position: center;
+        background-size: cover;
+        min-height: 100%;
+    }
+    .w3-bar .w3-button {
+        padding: 16px;
+    }
+
+
+
+
+    /* Remove the navbar's default margin-bottom and rounded borders */
+    .navbar {
+        margin-bottom: 0;
+        border-radius: 0;
+    }
+
+    /* Add a gray background color and some padding to the footer */
+    footer {
+        background-color: #f2f2f2;
+        padding: 25px;
+    }
+
+    .carousel-inner img {
+        width: 100%; /* Set width to 100% */
+        margin: auto;
+        min-height:200px;
+    }
+
+    /* Hide the carousel text when the screen is less than 600 pixels wide */
+    @media (max-width: 600px) {
+        .carousel-caption {
+            display: none;
+        }
+    }
+
 </style>
-<body class="w3-content" style="max-width:1200px">
-<?php include '../header.php';?>
-<!-- Sidebar/menu -->
-<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
-    <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold;margin-top:60px">
-       <?php include 'services_link.php';?>
-    </div>
-</nav>
+<body>
 
-<!-- Top menu on small screens -->
+<!-- Navbar (sit on top) -->
+<?php include 'header1.php';?>
 
-<!-- Overlay effect when opening sidebar on small screens -->
-<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
 
-<!-- !PAGE CONTENT! -->
-<div class="w3-main" style="margin-left:250px">
+<!-- Header with full-height image -->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
 
-    <!-- Push down content on small screens -->
-    <div class="w3-hide-large" style="margin-top:83px"></div>
+    <!--
 
-    <!-- Top header -->
-      <div class="container" align="left" style="margin:0px 300px">
-           <div class="row">
-           </br>
-		   </br>
-           <h1><b></b></h1>
-           </div>
-      </div>
-      <div class="container" align="left" style="margin:20px 50px">
-           <div class="row">
-           </br>
-		   </br>
-           <h4><b>3d Printing</b></h4>
-           </div>
-      </div>
-	
-<div id="myCarousel" class="carousel slide" data-ride="carousel" align="left" style="margin:50px">
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
 
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-    </ol>	
-	   <div class="carousel-inner" role="listbox">
 
         <div class="item active">
-            <img src="img/banner1.jpg" alt="Image">
+            <img src="img/slider/img13.jpg" alt="Image">
             <div class="carousel-caption">
-               <!-- <h1>MAHARASHTRA VOCATIONAL TRAINING CENTER</h1>
-                <p> Affiliated to Maharashtra State Board of Vocational Education Examination, Mumbai </p>
-                <h3>Institute Code: 170523</h3> -->
+                <h3 style="font-weight: bolder;">WE ARE HERE</h3>
+                <h1 style="font-weight: bolder;">TO MAKE ANYTHING YOU WANT</h1>
+                <button type="button" class="btn btn-primary">REQUEST A QUOTE</button>
             </div>
         </div>
 
         <div class="item">
-            <img src="img/banner2.jpg" alt="Image">
+            <img src="img/slider/img1.jpg" alt="Image">
             <div class="carousel-caption">
-                <!--<h1>MAHARASHTRA VOCATIONAL TRAINING CENTER</h1>
-                <p> Affiliated to Maharashtra State Board of Vocational Education Examination, Mumbai </p>
-                <h3>Institute Code: 170523</h3> -->
+                <h3 style="font-weight: bolder;">WE ARE HERE</h3>
+                <h1 style="font-weight: bolder;">TO MAKE ANYTHING YOU WANT</h1>
+                <button type="button" class="btn btn-primary">REQUEST A QUOTE</button>
             </div>
         </div>
 
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
-   </div>
-	  
-	  
-	  
-      <div class="container" align="left" style="margin:0px 50px">
-           <div class="row">
-           <p>Technology has made everything simple and easy to understand. Explore the new standerds in 3d printing industry. If you want to make an actual prototype (3d physical object/part) of your 3d CAD model file, IPRO3D is the right place which offers authentic materials and processes for 3d printing models/designs. We make sure that everything you get is of excellent qualityWhat is 3d printing? Glad you asked.
-Additive manufacturing process is popularly known as 3d printing. Additive manufacturing is a method of manufacturing physical parts in a successive addition process of adding material layer by layer but only one layer at a time on top of each other layer to build a physical part from a digital 3d CAD file.  The additive manufacturing principal is totally opposite to the traditional subtractive manufacturing using lathes, CNC’s, mills, etc.
-</br></br>Additive manufacturing has several different methods/processes of producing a prototype. Although each method/process has its own advantages but the principle of additive manufacturing or the popularly known name 3d printing remains the same.
-</br></br>The Idea of building a physical part by adding material rather than cutting away material applies no matter whichever additive manufacturing process you choose to use.
-</br></br>Step one
-First of all you will need an .STL file to upload. This file is generated by exporting or by saving you digital 3d CAD model in STL format. This file is a digital file of your 3d CAD model that you want to get 3d printed as a physical 3d object/part. Don’t worry isn’t as scary as it sounds.
-</br></br>These are several other ways to get 3d models</br>
-1)	3D scans your object: 3d scanning is not an easy process and generally requires a professional with 3d scanning equipment.</br>
-2)	CAD software: Computer Aided Design (CAD) has come a long way and has developed drastically which made it very much easy for you to 3d model/design your parts yourself.</br>
-3)	Download free designs: you can download free designs online IPRO3D marketplace offers free and paid designs for 3d printing and get it 3d printed from IPRO3D.</br>
-4)	Hire an expert designer:  If you don’t know 3d CAD designing/modeling or  require your design to be taken to next level, you can choose one of our Indian IPRO3D member expert designer to 3d CAD design/model your idea and make it ready for 3d printing.</br>
+        <div class="item">
+            <img src="img/slider/slide-2.jpg" alt="Image">
+            <div class="carousel-caption">
+                <h3 style="font-weight: bolder;">WE ARE HERE</h3>
+                <h1 style="font-weight: bolder;">TO MAKE ANYTHING YOU WANT</h1>
+                <button type="button" class="btn btn-primary">REQUEST A QUOTE</button>
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <!-- How we work  -->
+    <div class="container-fluid text-center">
+        <h3 style="font-weight: bolder;color:darkslategrey;">How We Work</h3>
+        <div class="row">
+            <div class="col-sm-2">
+                <img src="img/logo/idea.png">
+                <h5 style="font-weight: bold;">
+                    IDEA
+                </h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="img/logo/3dCAD.png">
+                <h5 style="font-weight: bold;">3D CAD<br>
+                    Model in<br>
+                    STL Format</h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="img/logo/stlUPload.png">
+                <h5 style="font-weight: bold;">
+                    Upload
+                    Design STL
+                    Files on
+                    website or<br>
+                    email it to us
+                </h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="img/logo/getQUta.png">
+                <h5 style="font-weight: bold;">
+                    Get Quotation
+                </h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="img/logo/3dadative.png">
+                <h5 style="font-weight: bold;">
+                    3D Printing<br>
+                    Additive
+                    manufacturing
+                    your product
+                </h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="img/logo/delivery.png">
+                <h5 style="font-weight: bold;">
+                    Delivery
+                </h5>
+            </div>
+
+        </div>
+
+    </div>
+
+    <!--  -->
+    <hr>
+    <!-- PRODUCTIOn  -->
+    <div class="container-fluid text-center">
+        <h3 style="font-weight: bolder;color:darkslategrey;">Prototype Your Product</h3>
+        <div class="row">
+            <div class="col-sm-2">
+                <img src="img/New%20folder/material.png" style="height: 150px;">
+                <h5 style="font-weight: bold;">Material</h5>
+                <h5>
+                    Select materials
+                    from a wide<br>
+                    variety in plastics,<br>
+                    metals, metal
+                    alloys and plastic
+                    composites
+                </h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="img/New%20folder/quality.png"  style="height: 150px;">
+                <h5 style="font-weight: bold;">Quality</h5>
+                <h5 >We don’t<br>
+                    compromise on
+                    part / product
+                    quality</h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="img/New%20folder/price.png" style="height: 150px;">
+                <h5 style="font-weight: bold;">Price </h5>
+                <h5>
+                    We provide the
+                    best competitive
+                    price compared
+                    to any other
+                    competitor in
+                    India and world
+                    3D printing
+                    market.
+                </h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="img/New%20folder/profuction.png" style="height: 150px;">
+                <h5 style="font-weight: bold;">Production</h5>
+                <h5>
+                    We manufacture all
+                    products at a time
+                    and help reduce the
+                    manufacturing time
+                    and cost while
+                    maintaining the
+                    optimum part quality
+                </h5>
+            </div>
+            <div class="col-sm-2">
+                <img src="img/New%20folder/delivery.png" style="height: 150px;">
+                <h5 style="font-weight: bold;">Delivery all
+                    over India </h5>
+                <h3>
+
+                </h3>
+            </div>
+            <div class="col-sm-2">
+                <img src="img/New%20folder/world.png" style="height: 150px;">
+                <h5 style="font-weight: bold;">World wide
+                    delivery</h5>
+                <h3>
+
+                </h3>
+            </div>
+
+        </div>
+
+    </div>
+
+    <!--  -->
+    <!-- Promo Section "Statistics" -->
+
+    <div class="w3-container w3-row w3-center " style="background-color: #f2f2f2;">
+        <h3 class="text-center" style="font-weight: bold;color: darkslategrey;">Statistics </h3>
+        <div class="w3-quarter w3-padding-44">
+            <span class="w3-xxlarge">14+</span>
+            <br><b>Partners</b>
+        </div>
+        <div class="w3-quarter w3-padding-44">
+            <span class="w3-xxlarge">55+</span>
+            <br><b>Projects Done</b>
+        </div>
+        <div class="w3-quarter w3-padding-44">
+            <span class="w3-xxlarge">89+</span>
+            <br><b>Happy Clients</b>
+        </div>
+        <div class="w3-quarter w3-padding-44">
+            <span class="w3-xxlarge">150+</span>
+            <br><b>Meetings</b>
+            <br><br><br><br>
+        </div>
+
+    </div>
 
 
 
+    <!-- About Section -->
+    <div class="w3-container"  id="about">
+        <h3 class="w3-center" style="font-weight: bold; color: darkslategrey;">Our Clients</h3>
+        <div class="w3-row-padding w3-center" style="margin-top:64px">
+            <div class="w3-quarter">
+                <img src="img/banner2.jpg" class="img-circle" style="height: 100px;width: 150px;">
+            </div>
+            <div class="w3-quarter">
+                <img src="img/banner2.jpg" class="img-circle" style="height: 100px;width: 150px;">
+            </div>
+            <div class="w3-quarter">
+                <img src="img/banner2.jpg" class="img-circle" style="height: 100px;width: 150px;">
+            </div>
+            <div class="w3-quarter">
+                <img src="img/banner2.jpg" class="img-circle" style="height: 100px;width: 150px;">
+            </div>
+        </div>
+    </div>
+    <br>
 
-           </div>
-      </div>
-	  <!-- Footer -->
-    <footer class="w3-padding-64 w3-light-grey w3-small w3-center" id="footer">
-
-
-
-    </footer>
-
-
-
-    <!-- End page content -->
-</div>
-
-<!-- Newsletter Modal -->
-
-<script>
-    // Accordion
-    function myAccFunc() {
-        var x = document.getElementById("demoAcc");
-        if (x.className.indexOf("w3-show") == -1) {
-            x.className += " w3-show";
-        } else {
-            x.className = x.className.replace(" w3-show", "");
-        }
-    }
-
-    // Click on the "Jeans" link on page load to open the accordion for demo purposes
-    document.getElementById("myBtn").click();
-
-
-    // Script to open and close sidebar
-    function w3_open() {
-        document.getElementById("mySidebar").style.display = "block";
-        document.getElementById("myOverlay").style.display = "block";
-    }
-
-    function w3_close() {
-        document.getElementById("mySidebar").style.display = "none";
-        document.getElementById("myOverlay").style.display = "none";
-    }
-</script>
+    <!-- Contact Section -->
+    <div class="row" style="background-color: #bbbbbb;">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3752.2523532716305!2d75.31352311425753!3d19.871563331647998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bdb99e84cc7f4e7%3A0x8ef52048513cb04!2sIPRO3D+Technologies+LLP!5e0!3m2!1sen!2sin!4v1534930979184" style="width:100%;" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+    <?php include 'footer.php';?>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
+    <!--
+    To use this code on your website, get a free API key from Google.
+    Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
+    -->
 
 </body>
 </html>
