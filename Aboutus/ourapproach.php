4512535c01cf1fe4d8458a1670dda0367bfa3384
gauravkad8 <kad.gaurav.gaurav447@gmail.com>
2018-08-23 20:34:41 +0530

    our approach

--- Aboutus/ourapproach.php
+++ Aboutus/ourapproach.php
@@ -16,96 +16,31 @@
 <link href="vGraphic.css" rel="stylesheet" type="text/css">
 <link href="whatsApp.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
-<style>
-    .w3-sidebar a {font-family: "Roboto", sans-serif}
-    body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
-</style>
-<body class="w3-content" style="max-width:1200px">
+<body>
 <?php include '../header.php';?>
-<!-- Sidebar/menu -->
-<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px;height:450px;" id="mySidebar">
-    <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold;margin-top:60px">
-       <?php include 'aboutus_link.php';?>
-    </div>
-</nav>
+<br><br><br>
+      <div class="container" style="color: darkslategrey;">
+           <div class="row" style="background-color: whitesmoke;padding: 20px;text-align: justify;">
+<h1 style="text-align: center; font-weight: bolder;">Our Approach</h1>
 
-<!-- Top menu on small screens -->
-<header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
-    <div class="w3-bar-item w3-padding-24 w3-wide"><img src="img/logo.png"></div>
-    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
-</header>
-
-<!-- Overlay effect when opening sidebar on small screens -->
-<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-
-<!-- !PAGE CONTENT! -->
-<div class="w3-main" style="margin-left:250px">
-
-    <!-- Push down content on small screens -->
-    <div class="w3-hide-large" style="margin-top:83px"></div>
-
-    <!-- Top header -->
-      <div class="container">
-           <div class="row">
-<br>
-		   <br>
-		   <br><br><br>
-<h1>Our Approach</h1>
-<br>
-
-<p style="margin-left:120px 0px"><img src="img/overview/1.png" align="right" Style="margin-left:120px 0px"></img>
- &nbsp;&nbsp;&nbsp;&nbsp;Our approach is to work towards bringing every innovative idea or product to reality (Actual 3d object/part)<br>
- by using a disruptive technology of Additive Manufacturing also popularly known as 3D printing and make the <br>
- world a better place to live by changing the face of manufacturing industry by removing its currently involved <br>
+ &nbsp;<p style="font-weight: bolder;">
+    <br>
+    &nbsp;&nbsp;&nbsp;Our approach is to work towards bringing every innovative idea or product to reality (Actual 3d object/part)
+ by using a disruptive technology of Additive Manufacturing also popularly known as 3D printing and make the
+ world a better place to live by changing the face of manufacturing industry by removing its currently involved
  hierarchy of traditional manufacturing and to become the global leader of digital manufacturing industry.<br><br>
 &nbsp;&nbsp;&nbsp;&nbsp;We work with Additive Manufacturing technology majorly involved in Industry 4.0. As Additive Manufacturing
-<br> is an integral part of Industry 4.0 the 4th industrial revolution.<br></p>
-<br><br>
+     is an integral part of Industry 4.0 the 4th industrial revolution.</p>
 
-
+               <br>
            </div>
 	</div>
-
-    
-
-
-    <!-- End page content -->
-</div>
-
-<!-- Newsletter Modal -->
-
-<script>
-    // Accordion
-    function myAccFunc() {
-        var x = document.getElementById("demoAcc");
-        if (x.className.indexOf("w3-show") == -1) {
-            x.className += " w3-show";
-        } else {
-            x.className = x.className.replace(" w3-show", "");
-        }
-    }
-
-    // Click on the "Jeans" link on page load to open the accordion for demo purposes
-    document.getElementById("myBtn").click();
-
-
-    // Script to open and close sidebar
-    function w3_open() {
-        document.getElementById("mySidebar").style.display = "block";
-        document.getElementById("myOverlay").style.display = "block";
-    }
-
-    function w3_close() {
-        document.getElementById("mySidebar").style.display = "none";
-        document.getElementById("myOverlay").style.display = "none";
-    }
-</script>
-<footer id="footer">
-<div class="container">
+<br><br>
+<div class="container-fluid">
+    <div class="row">
     <?php include'../footer.php';?>
+    </div>
 </div>
-    </footer>
 
 </body>
 </html>
