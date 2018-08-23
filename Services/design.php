6723d2b52161477a39e82e614d3667c524470b8a
gauravkad8 <kad.gaurav.gaurav447@gmail.com>
2018-08-23 23:32:33 +0530

    design.php

--- Services/design.php
+++ Services/design.php
@@ -19,62 +19,22 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 
-<style>
-    .w3-sidebar a {font-family: "Roboto", sans-serif}
-    body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
-</style>
-<body class="w3-content" style="max-width:1200px">
+<body>
+<?php include '../header.php';?>
+<br><br><br>
 
-<!-- Sidebar/menu -->
-<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
-    <div class="w3-container w3-display-container w3-padding-16">
-        <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
-        <h3 class="w3-wide"><b><img src="img/logo.png"></b></h3>
-    </div>
-    <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
-       <?php include 'template_link.php';?>
-     </div>
+<div class="container">
+    <div class="row">
 
-</nav>
+           <h3 style="text-align: center;"><b>Design for 3D printing </b></h3>
 
-<!-- Top menu on small screens -->
-<header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
-    <div class="w3-bar-item w3-padding-24 w3-wide"><img src="img/logo.png"></div>
-    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
-</header>
-
-<!-- Overlay effect when opening sidebar on small screens -->
-<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-
-<!-- !PAGE CONTENT! -->
-<div class="w3-main" style="margin-left:250px">
-
-    <!-- Push down content on small screens -->
-    <div class="w3-hide-large" style="margin-top:83px"></div>
-
-    <!-- Top header -->
-      <div class="container" align="left" style="margin:0px 300px">
-           <div class="row">
-           </br>
-		   </br>
-           <h1><b></b></h1>
-           </div>
-      </div>
-      <div class="container" align="left" style="margin:20px 50px">
-           <div class="row">
-           </br>
-		   </br>
-           <h4><b>Design for 3D printing </b></h4>
-           </div>
-      </div>
-	
-<div id="myCarousel" class="carousel slide" data-ride="carousel" align="left" style="margin:50px">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
 
     <!-- Indicators -->
     <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
         <li data-target="#myCarousel" data-slide-to="1"></li>
-    </ol>	
+    </ol>
 	   <div class="carousel-inner" role="listbox">
 
         <div class="item active">
@@ -106,55 +66,20 @@
     </a>
 </div>
    </div>
-	  
-	  
-	  
-      <div class="container" align="left" style="margin:0px 50px">
-           <div class="row">
-           <p>Design for 3d printing is a designing process through which the part can be design according to its final use or purpose but can be optimized for easy 3d printing for reducing the chance of any errors or defects occurrence during the 3d printing process or after production of final part.</br></br>
+        <br><br>
+<p style="font-weight: bolder;text-align: justify;">
 
+ Design for 3d printing is a designing process through which the part can be design according to its final use or purpose but can be optimized for easy 3d printing for reducing the chance of any errors or defects occurrence during the 3d printing process or after production of final part.</br></br>
+</p>
            </div>
       </div>
 	  <!-- Footer -->
-    <footer class="w3-padding-64 w3-light-grey w3-small w3-center" id="footer">
 
-
-
-    </footer>
-
-
-
-    <!-- End page content -->
+<div class="container-fluid">
+    <div class="row">
+        <?php include '../footer.php';?>
+    </div>
 </div>
-
-<!-- Newsletter Modal -->
-
-<script>
-    // Accordion
-    function myAccFunc() {
-        var x = document.getElementById("demoAcc");
-        if (x.className.indexOf("w3-show") == -1) {
-            x.className += " w3-show";
-        } else {
-            x.className = x.className.replace(" w3-show", "");
-        }
-    }
-
-    // Click on the "Jeans" link on page load to open the accordion for demo purposes
-    document.getElementById("myBtn").click();
-
-
-    // Script to open and close sidebar
-    function w3_open() {
-        document.getElementById("mySidebar").style.display = "block";
-        document.getElementById("myOverlay").style.display = "block";
-    }
-
-    function w3_close() {
-        document.getElementById("mySidebar").style.display = "none";
-        document.getElementById("myOverlay").style.display = "none";
-    }
-</script>
 
 </body>
 </html>
