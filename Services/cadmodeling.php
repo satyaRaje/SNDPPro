cf0822eea1c9d436ef2139ae125cbe070189fd88
gauravkad8 <kad.gaurav.gaurav447@gmail.com>
2018-08-23 23:22:15 +0530

    cad 3 d modling

--- Services/cadmodeling.php
+++ Services/cadmodeling.php
@@ -18,115 +18,69 @@
 <link href="whatsApp.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
-
-<style>
-    .w3-sidebar a {font-family: "Roboto", sans-serif}
-    body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
-</style>
-<body class="w3-content" style="max-width:1200px">
-
-<!-- Sidebar/menu -->
-<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
-    <div class="w3-container w3-display-container w3-padding-16">
-        <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
-        <h3 class="w3-wide"><b><img src="img/logo.png"></b></h3>
-    </div>
-    <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
-       <?php include 'template_link.php';?>
-     </div>
-
-</nav>
-
-<!-- Top menu on small screens -->
-<header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
-    <div class="w3-bar-item w3-padding-24 w3-wide"><img src="img/logo.png"></div>
-    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
-</header>
-
-<!-- Overlay effect when opening sidebar on small screens -->
-<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-
-<!-- !PAGE CONTENT! -->
-<div class="w3-main" style="margin-left:250px">
-
-    <!-- Push down content on small screens -->
-    <div class="w3-hide-large" style="margin-top:83px"></div>
-
-    <!-- Top header -->
-      <div class="container" align="left" style="margin:0px 300px">
+<body style="background-color: whitesmoke;color: darkslategrey;">
+<?php include '../header.php';?>
+<br><br><br>
+      <div class="container">
            <div class="row">
-           </br>
-		   </br>
-           <h1><b></b></h1>
-           </div>
-      </div>
-      <div class="container" align="left" style="margin:20px 50px">
-           <div class="row">
-           </br>
-		   </br>
-           <h4><b>3D and 2D CAD Modelling/Designing:</b></h4>
+           <h3 style="text-align: center; "><b>3D and 2D CAD Modelling/Designing:</b></h3>
+               <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+                   <!-- Indicators -->
+                   <ol class="carousel-indicators">
+                       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                       <li data-target="#myCarousel" data-slide-to="1"></li>
+                   </ol>
+                   <div class="carousel-inner" role="listbox">
+
+                       <div class="item active">
+                           <img src="img/banner1.jpg" alt="Image">
+                           <div class="carousel-caption">
+                               <!-- <h1>MAHARASHTRA VOCATIONAL TRAINING CENTER</h1>
+                                <p> Affiliated to Maharashtra State Board of Vocational Education Examination, Mumbai </p>
+                                <h3>Institute Code: 170523</h3> -->
+                           </div>
+                       </div>
+
+                       <div class="item">
+                           <img src="img/banner2.jpg" alt="Image">
+                           <div class="carousel-caption">
+                               <!--<h1>MAHARASHTRA VOCATIONAL TRAINING CENTER</h1>
+                               <p> Affiliated to Maharashtra State Board of Vocational Education Examination, Mumbai </p>
+                               <h3>Institute Code: 170523</h3> -->
+                           </div>
+                       </div>
+
+                       <!-- Left and right controls -->
+                       <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                           <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                           <span class="sr-only">Previous</span>
+                       </a>
+                       <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                           <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                           <span class="sr-only">Next</span>
+                       </a>
+                   </div>
+               </div>
            </div>
       </div>
 	
-<div id="myCarousel" class="carousel slide" data-ride="carousel" align="left" style="margin:50px">
 
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-    </ol>	
-	   <div class="carousel-inner" role="listbox">
-
-        <div class="item active">
-            <img src="img/banner1.jpg" alt="Image">
-            <div class="carousel-caption">
-               <!-- <h1>MAHARASHTRA VOCATIONAL TRAINING CENTER</h1>
-                <p> Affiliated to Maharashtra State Board of Vocational Education Examination, Mumbai </p>
-                <h3>Institute Code: 170523</h3> -->
-            </div>
-        </div>
-
-        <div class="item">
-            <img src="img/banner2.jpg" alt="Image">
-            <div class="carousel-caption">
-                <!--<h1>MAHARASHTRA VOCATIONAL TRAINING CENTER</h1>
-                <p> Affiliated to Maharashtra State Board of Vocational Education Examination, Mumbai </p>
-                <h3>Institute Code: 170523</h3> -->
-            </div>
-        </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
-   </div>
 	  
 	  
 	  
-      <div class="container" align="left" style="margin:0px 50px">
-           <div class="row">
-           <p>3D and 2d Computer Aided Designing is a technology for designing and technical documentation for parts/models, which replaces manual drafting process. It is widely used by architects, engineers, and other professionals. 3D CAD modeling provides an extra dimension to precisely visualize and share designs concepts and iterate it many times in real time.</br></br>
+      <div class="container">
+           <div class="row" style="font-weight: bolder;">
+           3D and 2d Computer Aided Designing is a technology for designing and technical documentation for parts/models, which replaces manual drafting process. It is widely used by architects, engineers, and other professionals. 3D CAD modeling provides an extra dimension to precisely visualize and share designs concepts and iterate it many times in real time.</br></br>
            </div>
       </div>
 	  <!-- Footer -->
-    <footer class="w3-padding-64 w3-light-grey w3-small w3-center" id="footer">
 
-
-
-    </footer>
-
-
-
-    <!-- End page content -->
-</div>
-
-<!-- Newsletter Modal -->
+ <div class="container-fluid">
+     <div class="row">
+         <?php include '../footer.php';?>
+     </div>
+ </div>
 
 <script>
     // Accordion
