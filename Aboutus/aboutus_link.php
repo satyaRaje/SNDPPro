26744943212b0fb33a0fe2041747fa3180588457
gauravkad8 <kad.gaurav.gaurav447@gmail.com>
2018-09-05 11:56:03 +0530

    about us link update

--- Aboutus/aboutus_link.php
+++ Aboutus/aboutus_link.php
@@ -10,17 +10,49 @@
             background-color: transparent;
         }
 
-    .dropdown:hover .dropbtn {background-color: transparent;}
-    .dropbtn:hover{
+
+
+    .dropbtn {
+        color: black;
         background-color: transparent;
+        padding: 16px;
+        font-size: 15px;
+        border: none;
     }
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
+
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f1f1f1;
+        min-width: 160px;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 1;
+    }
+
+    .dropdown-content a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
+
+    .dropdown-content a:hover {background-color: #ddd;}
+
+    .dropdown:hover .dropdown-content {display: block;}
+
+    .dropdown:hover .dropbtna {background-color: transparent;}
+
 
     </style>
 </head>
 <body>
 <div class="dropdown">
 
-        <button class="dropbtn"><img src="../img/svg/menu.svg" class="floattu" height="50px" style="float: left;background-color: transparent;"> </button>
+        <button class="dropbtna"><img src="../img/svg/menu.svg" class="floattu" height="50px" style="float: left; "> </button>
         <div class="dropdown-content">
             <!--    <a href="../Aboutus/aboutus.php">About Us</a>-->
             <a href="#"></a>
