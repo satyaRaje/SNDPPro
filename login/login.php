58f66c09b1c429dc1047a6182b4fa2568ab0bdaa
deepika-sodhi <deepikasodhi5@gmail.com>
2018-09-16 12:17:06 -0400

    message13

--- login/login.php
+++ login/login.php
@@ -8,7 +8,7 @@
     <style>
         .login-block{
             background: #DE6262;  /* fallback for old browsers */
-            background: -webkit-linear-gradient(to bottom, #FFB88C, #DE6262);  /* Chrome 10-25, Safari 5.1-6 */
+            background: -webkit-linear-gradient(to bottom, #DCDCDC, #DCDCDC);  /* Chrome 10-25, Safari 5.1-6 */
             background: linear-gradient(to bottom, #FFB88C, #DE6262); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
             float:left;
             width:100%;
