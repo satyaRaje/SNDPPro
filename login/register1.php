ba59d275282cd31161d7b07a880a7bbee502cd1d
swara <swaranjalik24@gmail.com>
2018-09-19 01:09:36 +0530

    updated login and register page

--- login/register1.php
+++ login/register1.php
@@ -12,7 +12,7 @@
                         <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt=""/>
                         <h3>Welcome</h3>
                         <p>You are 30 seconds away from earning your own money!</p>
-                        <input type="submit" name="" value="Login"/><br/>
+                        <a href="login2.php"><input type="submit" name="" value="Login"/></a><br/>
                     </div>
                     <div class="col-md-9 register-right">
                         <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
