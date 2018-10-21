0971ff46c6e05d88240075fd98ed0f4263fb5eb2
swara <swaranjalik24@gmail.com>
2018-10-21 16:10:05 +0530

    marketplace page

---
+++ marketplace.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<link href="vGraphic.css" rel="stylesheet" type="text/css">
+<link href="whatsApp.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+
+<style>
+#myDIV {
+     background: #DCDCDCDC	;
+    -webkit-animation: mymove 5s infinite; /* Safari 4.0 - 8.0 */
+    animation: mymove 5s infinite;
+}
+
+/* Safari 4.0 - 8.0 */
+@-webkit-keyframes mymove {
+    50% {background-color: #D3D3D3;}
+}
+
+@keyframes mymove {
+    50% {background-color: #D3D3D3;}
+}
+
+#rcorners1 {
+    border-radius: 25px;
+    background: #2ACFEE;
+    padding: 20px; 
+    width: 200px;
+    height: 150px;    
+}
+
+.card {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    width: 90%;
+}
+
+.card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+    padding: 2px 16px;
+}
+
+</style>
+
+
+<body>
+
+<div class="container-fluid">
+<?php include 'header1.php';?>
+
+<div class="container-fluid">
+    <div class="row">
+</br></br></br>
+</div>
+</div>
+
+<div class="container-fluid" style="margin-right:0%;">
+    <div class="row" >
+	  <div class="col-sm-9">
+       </br></br></br>
+	   <button id="rcorners1" style="margin-left:4%">Upload Your Product !</button> 	   
+	   <button id="rcorners1" style="margin-left:5%">Sale Your Idea !</button>
+	   <button id="rcorners1" style="margin-left:5%">Hier a Designer !</button>
+	   <button id="rcorners1" style="margin-left:5%">Rounded corners!</button></br></br></br>
+	      
+<div class="container-fluid" style="margin-right:0%;">
+    <div class="row" > 
+		<div class="col-sm-3">  
+           <div class="card">
+             <center><img src="img/team/t4.jpg" class="card-img-top img-circle"  height="160px" width="150px"></center>
+             <div class="container">
+             <p style="padding-left:35px">Architect & Engineer</p> 
+             </div>
+	        </div>
+        </div>
+		<div class="col-sm-3">
+		   <div class="card">
+             <center><img src="img/team/t4.jpg" class="card-img-top img-circle"  height="160px" width="150px"></center>
+             <div class="container">
+             <p style="padding-left:35px">Architect & Engineer</p> 
+             </div>
+           </div>
+		</div>
+		<div class="col-sm-3">
+		   <div class="card">
+             <center><img src="img/team/t4.jpg" class="card-img-top img-circle"  height="160px" width="150px"></center>
+             <div class="container">
+             <p style="padding-left:35px">Architect & Engineer</p> 
+             </div>
+           </div>
+		</div>
+		<div class="col-sm-3">
+		   <div class="card">
+             <center><img src="img/team/t4.jpg" class="card-img-top img-circle"  height="160px" width="150px"></center>
+             <div class="container">
+             <p style="padding-left:35px">Architect & Engineer</p> 
+             </div>
+           </div>
+		</div>
+		   
+	</div>
+   </div>
+ </div>
+	   
+	  <div class="col-sm-3" id="myDIV" style="height:700px">
+	  </br>
+	  <center><img src="img/team/t4.jpg" class="card-img-top img-circle" height="160px" width="150px"></center>
+        </br>
+		<div align="center">
+		<h3>Profile </br></h3>
+         Name - 
+		</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
