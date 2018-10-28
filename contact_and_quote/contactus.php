ba0a840bf4d340781325893ce773811d5441951c
data <joshipoonam207@gmail.com>
2018-10-28 06:04:16 -0500

    register and login form

--- contact_and_quote/contactus.php
+++ contact_and_quote/contactus.php
@@ -1,91 +1,68 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-	
-<title>IPRO3D-Contact_Us</title>
-<style>
-    body{
-        width:100%;
-        height:100%;
-        font-family: 'Roboto Condensed', sans-serif;
+    <!-- Title Page-->
+    <title>IPRO3D Contact Here</title>
 
-    }
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
 
-
-    h1,h2,h3 {
-        margin:0 0 35px 0;
-        font-family: 'Patua One', cursive;
-        text-transform: uppercase;
-        letter-spacing:1px;
-		color:black;
-    }
-
-    p{
-        margin:0 0 25px;
-        font-size:18px;
-        line-height:1.6em;
-		color:black;
-    }
-    a{
-        color:black;
-    }
-    a:hover,a:focus{
-        text-decoration:none;
-        color:black;
-    }
-
-    #contact{
-        background:#DCDCDC;
-        color:#333333;
-        color:#f4f4f4;
-        padding-bottom:80px;
-		color:black;
-    }
-
-    textarea.form-control{
-        height:100px;
-		color:black;
-    }
-</style>
-
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
 </head>
+
 <body>
-<div class="container-fluid">
-    <div class="row">
-        <?php include '../header.php';?>
-    </div>
-</div>
-<br><br><br>
-<form method="post" action="">
-    <section id="contact" class="content-section text-center">
-        <div class="contact-section">
-            <div class="container">
-                <h2>Contact Us</h2>
-                <p>Feel free to contact us by filling the contact form or visiting our social sites like Facebook,Whatsapp,Twitter,Instagram,LinkedIn.</p>
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
-                            <div class="form-group">
-                                <label for="exampleInputName2"></label>
-                                <input type="text" class="form-control" name="fname" placeholder="Enter your name">
+<div class="page-wrapper bg-dark p-t-100 p-b-50">
+    <div class="wrapper wrapper--w900">
+        <div class="card card-6">
+            <div class="card-heading">
+                <h2 class="title">Contact Us</h2>
+            </div>
+            <div class="card-body">
+                <form method="POST">
+                    <div class="form-row">
+                        <div class="name">Full name</div>
+                        <div class="value">
+                            <input class="input--style-6" type="text" name="full_name">
+                        </div>
+                    </div>
 
-                            </div>
+                    <div class="form-row">
+                        <div class="name">Email address</div>
+                        <div class="value">
+                            <input class="input--style-6" type="email" name="email" placeholder="example@email.com">
+                        </div>
+                    </div>
 
-                            <div class="form-group">
-                                <label for="exampleInputEmail2"></label>
-                                <input type="text" class="form-control" name="mobileno" maxlength="10" placeholder="Enter 10-digit Number">
-                            </div>
-                            
-                            <div class="form-group">
-                                <label for="exampleInputEmail2"></label>
-                                <input type="email" class="form-control" name="email" placeholder="abc@gmail.com">
-                            </div>
+                    <div class="form-row">
+                        <div class="name">Mobile Number</div>
+                        <div class="value">
+                            <input class="input--style-6" type="mobno" name="email" placeholder="Enter 10 Digit">
+                        </div>
+                    </div>
+
+
+                    <div class="form-row">
+                        <div class="name">Message</div>
+                        <div class="value">
+                            <textarea class="textarea--style-6" name="message" placeholder="Message sent to the Company"></textarea>
+                        </div>
+                    </div>
+
+
+                    <div class="form-row">
                         <div class="dropdown">
-                            <label for="exampleInputEmail2"></label>
-                            <select input type="text" class="form-control" name="info" maxlength="email" placeholder="Info">
-
+                            <div class="name">Designation</div>
+                            <div class="value">
+                                <select input type="input--style-6" class="form-control" name="info" maxlength="email" placeholder="Info">
                                     <option value="Student">Student</option>
                                     <option value="Industry">Industry</option>
                                     <option value="Employee">Employee</option>
@@ -94,66 +71,45 @@
                                     <option value="professional">Professional</option>
                                     <option value="other">Other</option>
                                 </select>
-                                    </div>
-<br><br>
-                        <div class="dropdown">
-                            <label for="exampleInputEmail2"></label>
-                            <select input type="text" class="form-control" name="info" maxlength="email" placeholder="How You Know us">
-
-                                <option value="social">Social Media</option>
-                                <option value="news">Newspaper</option>
-                                <option value="google">Google Search</option>
-                                <option value="youtube">YouTube</option>
-                                <option value="justdial">JustDial</option>
-                                <option value="friend">Friend</option>
-                                <option value="direct">Direct us</option>
-                                <option value="other">Other</option>
-                            </select>
-                        </div>
-<br><br>
-                        <div class="form-group ">
-                                <label for="exampleInputText"></label>
-                                <textarea  class="form-control" name="message" placeholder="Your Message(Description)"></textarea>
                             </div>
-                            <button type="submit" class="btn btn-default" name="enquire">Send Message</button>
+                        </div>
 
-                        <hr>
-                        <h3>Our Social Sites</h3>
-                        <ul class="list-inline banner-social-buttons">
-                            <li><a href=" https://twitter.com/ipro3d" class="btn btn-default btn-lg"><i class="fa fa-twitter"> <span class="network-name">Twitter</span></i></a></li>
-                            <li><a href="https://www.facebook.com/ipro3d.tech" class="btn btn-default btn-lg"><i class="fa fa-facebook"> <span class="network-name">Facebook</span></i></a></li>
-                            <li><a href="#" class="btn btn-default btn-lg"><i class="fa fa-instagram"> <span class="network-name">Instagram</span></i></a></li>
-                        </ul>
+                        <div class="form-row">
+                            <div class="dropdown">
+                                <div class="name">How You Know Us</div>
+                                <div class="value">
+                                    <select input type="input--style-6" class="form-control" name="info" maxlength="email" placeholder="How You Know us">
+                                        <option value="social">Social Media</option>
+                                        <option value="news">Newspaper</option>
+                                        <option value="google">Google Search</option>
+                                        <option value="youtube">YouTube</option>
+                                        <option value="justdial">JustDial</option>
+                                        <option value="friend">Friend</option>
+                                        <option value="direct">Direct us</option>
+                                        <option value="other">Other</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </form>
+            </div>
+
+            <div class="card-footer">
+                <button class="btn btn--radius-2 btn--blue-2" type="submit">Send Application</button>
             </div>
         </div>
-    </section>
-
-</form>
-
-
-<?php
-if(isset($_POST['enquire'])){
-  /*  $query= "INSERT INTO `tblEnquiry`(`name`, `mobile`, `email`, `course`) VALUES ('".$_POST['fname']."','".$_POST['mobileno']."','".$_POST['email']."','".$_POST['message']."')";
-    $conn = mysqli_connect("localhost","root", "","dbipro3d");
-    $check = mysqli_query($conn,$query);*/
-    $to = "hello@ipro3d.io";
-    $subject = "Email Enquiry : ";
-    $txt = "Student Name : ".$_POST['fname']." Requirement's : ".$_POST['message']." Mobile No. : ".$_POST['mobileno']." Email Address : ".$_POST['email'];
-    $headers = "From: mit@arkay.org" . "\r\n" .
-        "CC: kad.gaurav.gaurav447@gmail.com";
-    mail($to,$subject,$txt,$headers);
-
-    echo "<script>alert('Thanks For Contact...!');</script>";
-}
-
-?>
-<div class="container-fluid" style="background-color: black;color: whitesmoke;">
-    <div class="row">
-<?php include '../footer.php';?>
     </div>
 </div>
-</body>
+
+<!-- Jquery JS-->
+<script src="vendor/jquery/jquery.min.js"></script>
+
+
+<!-- Main JS-->
+<script src="js/global.js"></script>
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
 </html>
+<!-- end document-->
