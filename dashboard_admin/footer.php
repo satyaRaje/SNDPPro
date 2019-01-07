667fd4566e28c482b5896ac31ec50248d3731bac
gauravkad8 <kad.gaurav.gaurav447@gmail.com>
2019-01-07 10:05:13 +0530

    test

--- dashboard_admin/footer.php
+++
@@ -1,7 +0,0 @@
-<footer style="background-color: #bbbbbb;" class="container-fluid text-center">
-    <div class="container text-center">
-        <div class="row text-center">
-
-        </div>
-    </div>
-</footer>
--- login/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$web = "localhost";
-$user = "root";
-$pass = "";
-
-$conn = mysqli_connect("localhost",$user,$pass,'dbipro3d');
-
-?>
